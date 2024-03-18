77ce5ba9bb22ef7929f0e8d9ee273125ed465dbc
Jaguar00937 <163587472+Jaguar00937@users.noreply.github.com>
2024-03-18 20:07:29 +0530

    Add files via upload

---
+++ EvenOdd.c
@@ -0,0 +1,43 @@
+// #include<stdio.h>
+// int main(){
+//     int n;
+//     printf("enter a number:");
+//     scanf("%d",&n);
+//     if(n%2==0){
+//         printf("this is an even number");
+//     }else{
+//         printf("this is an odd number");
+//         return 0;
+//     }
+
+// }
+
+// or
+
+// #include<stdio.h>
+// int main(){
+//     int n;
+//     printf("enter a number:");
+//     scanf("%d",&n);
+//     if(n%2==0){
+//         printf("this is an even number");
+//     }
+//         if(n%2!=0){
+//         printf("this is an odd no ");
+
+//     }
+
+// }
+
+#include<stdio.h>
+int main(){
+    int n;
+    printf("enter any no: ");
+    scanf("%d",&n);
+    if(n%5==0){
+        printf("this is divisible by 5: ");
+        }else{
+            printf("this is not divisible by: 5 ");
+
+        }
+    }
